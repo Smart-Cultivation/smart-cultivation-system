b5a499777c7ceece41390135f1a2c7443ffac5d1
Smart-Cultivation <smart.cultivation.si@gmail.com>
2024-07-19 21:09:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 # load_dotenv()
 
 CONFIG = {
-    "SECRET_KEY": os.getenv("SECRET_KEY"),
+    "SECRET_KEY": os.environ.get("SECRET_KEY"),
     "CACHE_TYPE": "SimpleCache",
     "CACHE_DEFAULT_TIMEOUT": 300,
     "SESSION_TYPE": "filesystem",
-    "SQLALCHEMY_DATABASE_URI": os.getenv("POSTGRES_URL_NON_POOLING"),
+    "SQLALCHEMY_DATABASE_URI": os.environ.get("POSTGRES_URL_NON_POOLING"),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 }
