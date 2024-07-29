538fb8976db930669c2e231028751a49a0866051
fnilvuwu <mamatmks45@gmail.com>
2024-07-29 22:49:35 +0800

    fix error

--- config.py
+++ config.py
@@ -13,19 +13,10 @@ import os
 # }
 
 CONFIG = {
-    "SECRET_KEY": os.getenv("SECRET_KEY"),
+    "SECRET_KEY": os.environ.get("SECRET_KEY"),
     "CACHE_TYPE": "SimpleCache",
     "CACHE_DEFAULT_TIMEOUT": 300,
     "SESSION_TYPE": "filesystem",
-    "SQLALCHEMY_DATABASE_URI": (
-        "mysql+pymysql://"
-        + os.getenv("MYSQL_USERNAME")
-        + ":"
-        + os.getenv("MYSQL_PASSWORD")
-        + "@"
-        + os.getenv("MYSQL_HOST")
-        + "/"
-        + os.getenv("MYSQL_DATABASE")
-    ),
+    "SQLALCHEMY_DATABASE_URI": os.environ.get("POSTGRES_URL_NON_POOLING"),
     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 }
