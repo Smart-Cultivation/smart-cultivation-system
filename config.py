a2bb8330ee5ebe95afc609b0efaccc8e824daa6c
fnilvuwu <mamatmks45@gmail.com>
2024-07-30 23:45:42 +0800

    modified:   config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
-# import os
-from dotenv import dotenv_values
+import os
+# from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
-CONFIG = {
-    "SECRET_KEY": config["SECRET_KEY"],
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 300,
-    "SESSION_TYPE": "filesystem",
-    "SQLALCHEMY_DATABASE_URI": config["POSTGRES_URL_NON_POOLING"],
-    "SQLALCHEMY_TRACK_MODIFICATIONS": False,
-}
+# config = dotenv_values(".env")
 
 # CONFIG = {
-#     "SECRET_KEY": os.environ.get("SECRET_KEY"),
+#     "SECRET_KEY": config["SECRET_KEY"],
 #     "CACHE_TYPE": "SimpleCache",
 #     "CACHE_DEFAULT_TIMEOUT": 300,
 #     "SESSION_TYPE": "filesystem",
-#     "SQLALCHEMY_DATABASE_URI": os.environ.get("POSTGRES_URL_NON_POOLING"),
+#     "SQLALCHEMY_DATABASE_URI": config["POSTGRES_URL_NON_POOLING"],
 #     "SQLALCHEMY_TRACK_MODIFICATIONS": False,
 # }
+
+CONFIG = {
+    "SECRET_KEY": os.environ.get("SECRET_KEY"),
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300,
+    "SESSION_TYPE": "filesystem",
+    "SQLALCHEMY_DATABASE_URI": os.environ.get("POSTGRES_URL_NON_POOLING"),
+    "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+}
